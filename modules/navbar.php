006f4162745fdec52d7b26612a1eedc24321c5f2
Jaydeep Dharamsey <33755235+Jaydeep21@users.noreply.github.com>
2019-06-20 12:06:25 +0530

    Add files via upload

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,100 +1,57 @@
-<?php session_start();?>
-<html lang="en">
+<?php
+session_start();
+?>
+<html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
-  <style>
-      body{
-          margin:0;
-          padding:0;
-          font-family: sans-serif;
-      }
-      
-      nav{
-        width:100%;
-          background: black;
-      }
-      ul{
-          width:100%;
-          margin:0 auto;
-          padding: 0;
-      }
-      ul li{
-          list-style:none;
-          display: inline-block;
-          padding:20px;
-      }
-      ul li:hover{
-          background: red;
-      }
-      ul li a{
-          color:white;
-            text-decoration:none;
-      }
-      .toggle{
-          width:100%;
-          padding:10px 20px;
-          background: black;
-          text-align: :right;
-          box-sizing: border-box;
-          color:white;
-          font-size:30px;
-          display:none;
-      }
-      @media(max-width:768px){
-          .toggle{
-              display: block;
-              text-align: center;
-          }
-          ul{
-              width: 100%;
-              display: none;
-          }
-          ul li{
-              display: block;
-              text-align: center;
-          }
-          .active{
-              display: block;
-          }
-      }
-    </style>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
-<body>
-
-    <nav>
-        <div class="toggle">
-            <i class="fa fa-bars menu" aria-hidden="true"></i>
-        </div>
-        <ul>
-            <li><a href="http://localhost/SakecStudentsPortal/index.php">Home</a></li>
-            <li><a href="#">About</a>
-            <li><a href="#">FAQ's</a>
-            <li><a href="modules/ebooks.php">Ebooks</a>
-            <?php if(!isset($_SESSION['emailid'])){?>
-                <li style="float:right;">
-                    <a href="http://localhost/SakecStudentsPortal/modules/login.php">Login</a>
-                <li style="float:right;">
-                    <a href="http://localhost/SakecStudentsPortal/modules/signup.php">Signup</a>
-            <?php }else{ ?>
-            <li style="float:right;">
-                <a href="http://localhost/SakecStudentsPortal/modules/logout.php">Logout</a>
-            <li style="float:right;">
-                <a href="http://localhost/SakecStudentsPortal/modules/profile.php">Hello <?php echo $_SESSION['fname'];?></a>
-                <?php } ?>
-        </ul>
-        
-    </nav>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('.menu').click(function(){
-            $('ul').toggleClass('active');
-        })
-    })
-    </script>
-    
-</body>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark " >
+            <button class="navbar-toggler mx-auto" data-toggle="collapse" data-target="#collapse_target">
+                <span class="navbar-toggler-icon align-center"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapse_target">
+                <ul class="navbar-nav mr-auto ">
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="http://localhost/SakecStudentsPortal/index.php">Home</a>
+                    </li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="http://localhost/SakecStudentsPortal/index.php?#">About Us</a>
+                    </li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="http://localhost/SakecStudentsPortal/index.php?#">FAQ's</a>
+                    </li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="http://localhost/SakecStudentsPortal/modules/ebooks.php">Ebooks</a>
+                    </li>    
+                </ul>
+                <ul class="navbar-nav">
+                
+            
+                    
+                    <?php if(isset($_SESSION['userid'])){
+                        
+                    ?>
+                    <li class="nav-item p-2">
+                        <a class="nav-link " href="http://localhost/SakecStudentsPortal/modules/profile.php"><?php echo "Hello ",$_SESSION['fname']; ?></a>
+                    </li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link " href="http://localhost/SakecStudentsPortal/modules/logout.php">Logout</a>
+                    </li>
+                    <?php } else{?>
+                    <li class="nav-item p-2"><a class="nav-link "  href="http://localhost/SakecStudentsPortal/modules/signup.php">Signup</a></li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="http://localhost/SakecStudentsPortal/modules/login.php">Login</a> 
+                    </li>
+                    <?php }?>
+                </ul>
+            </div>    
+        </nav>
+    </body>
 </html>
